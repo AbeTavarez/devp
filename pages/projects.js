dc4e9689926a237edb15ce07137d615675567a9e
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-08-27 17:30:57 -0400

    nav quick fix

--- pages/projects.js
+++ pages/projects.js
@@ -6,7 +6,7 @@ export default function Projects() {
       className={`flex justify-center items-center w-full h-screen  relative bg-gradient-to-r from-indigo-500 to-indigo-800 md:flex-col dark:bg-gradient-to-r dark:from-slate-700 dark:to-slate-950`}
     >
       <div className="font-bold text-2xl text-center leading-loose">
-        <div className="m-20">
+        <div>
           <h1>Projects Page</h1>
           <h1>Sorry, this page is being redesigned &#127912;</h1>
           <h1>Please comeback soon to see the new content!</h1>
