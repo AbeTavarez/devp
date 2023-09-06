1696c9f67cb786cf2e5d487c6591e815b8746ac8
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-09-05 21:08:49 -0400

    projects page updated

--- pages/projects.js
+++ pages/projects.js
@@ -1,86 +1,114 @@
 import { AppLayout } from "../components/AppLayout";
+import { faChalkboardUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Projects() {
   return (
-    <main
-      className={`flex justify-center items-center w-full h-screen  relative bg-gradient-to-r from-indigo-500 to-indigo-800 md:flex-col dark:bg-gradient-to-r dark:from-slate-700 dark:to-slate-950`}
-    >
-      <div className="font-bold text-2xl text-center leading-loose">
-        <div>
-          <h1>Projects Page</h1>
-          <h1>Sorry, this page is being redesigned &#127912;</h1>
-          <h1>Please comeback soon to see the new content!</h1>
-        </div>
+    <main className="w-full h-full bg-white dark:bg-gradient-to-r dark:from-slate-700 dark:to-slate-950 bg-gradient-to-r from-indigo-500 to-indigo-800 p-5">
+      <h1 className="text-white text-3xl p-10 font-bold dark:text-white underline">
+        Projects
+      </h1>
 
-        <h2 className="font-bold text-3xl text-center mb-5 underline">
-          Devle mobile App
+      <div class="max-w-2xl mx-auto">
+        <h2 className="font-bold text-3xl text-center mb-5 underline text-green-400">
+          Cross Platform Mobile Application{" "}
+          <span className="text-amber-500">(iOS & Android)</span>
         </h2>
-        <div class="max-w-2xl mx-auto">
-          <div class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <h3 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
-              Devle App
-            </h3>
-            <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-              Expand your tech knowledge by having fun with Devle on iOS &
-              Android. Download the app for free today.
-            </p>
-            <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-              <a
-                href="https://apps.apple.com/za/app/devle/id1659893831"
-                target="_blank"
-                class="w-full sm:w-auto flex bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+        <div class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <h3 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+            Devle App
+          </h3>
+          <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+            Expand your tech knowledge by having fun with Devle on iOS &
+            Android. Download the app for free today.
+          </p>
+          <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+            <a
+              href="https://apps.apple.com/za/app/devle/id1659893831"
+              target="_blank"
+              class="w-full sm:w-auto flex bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            >
+              <svg
+                class="mr-3 w-7 h-7"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="apple"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
               >
-                <svg
-                  class="mr-3 w-7 h-7"
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="apple"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 384 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-                  ></path>
-                </svg>
-                <div class="text-left">
-                  <div class="mb-1 text-xs">Download on the</div>
-                  <div class="-mt-1 font-sans text-sm font-semibold">
-                    Mac App Store
-                  </div>
+                <path
+                  fill="currentColor"
+                  d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
+                ></path>
+              </svg>
+              <div class="text-left">
+                <div class="mb-1 text-xs">Download on the</div>
+                <div class="-mt-1 font-sans text-sm font-semibold">
+                  Mac App Store
                 </div>
-              </a>
-              <a
-                href="https://play.google.com/store/apps/details?id=com.tacticalDevs.devle"
-                target="_blank"
-                class="w-full sm:w-auto flex bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+              </div>
+            </a>
+            <a
+              href="https://play.google.com/store/apps/details?id=com.tacticalDevs.devle"
+              target="_blank"
+              class="w-full sm:w-auto flex bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            >
+              <svg
+                class="mr-3 w-7 h-7"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="google-play"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
               >
-                <svg
-                  class="mr-3 w-7 h-7"
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="google-play"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"
-                  ></path>
-                </svg>
-                <div class="text-left">
-                  <div class="mb-1 text-xs">Get in on</div>
-                  <div class="-mt-1 font-sans text-sm font-semibold">
-                    Google Play
-                  </div>
+                <path
+                  fill="currentColor"
+                  d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"
+                ></path>
+              </svg>
+              <div class="text-left">
+                <div class="mb-1 text-xs">Get in on</div>
+                <div class="-mt-1 font-sans text-sm font-semibold">
+                  Google Play
                 </div>
-              </a>
-            </div>
+              </div>
+            </a>
           </div>
+        </div>
+      </div>
+
+      {/* BETA PROJECT  */}
+      <div class="max-w-2xl mx-auto my-52">
+        <h2 className="font-bold text-3xl text-center mb-5 underline text-green-400">
+          Beta SAAS Platform Application{" "}
+          <span className="text-amber-500">(ChatGPT-4)</span>
+        </h2>
+        <div class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center">
+          <h3 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+            With Compose Beta Version
+          </h3>
+          <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+            Chat GPT-4 AI-powered SAAS solution to help you get your tasks done
+            in minutes. Get high-quality content, without sacrificing your time.
+          </p>
+          <a
+            href="https://clownfish-app-23htu.ondigitalocean.app/"
+            target="_blank"
+          >
+            <div className="text-md text-white text-center py-4 font-heading bg-slate-900 w-48 rounded hover:cursor-pointer">
+              <FontAwesomeIcon
+                icon={faChalkboardUser}
+                className="text-2xl text-white"
+              />
+              <span className="text-white ml-5 font-sans text-sm font-semibold ">
+                With Compose{" "}
+              </span>
+            </div>
+          </a>
         </div>
       </div>
     </main>
