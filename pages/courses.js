78978b94ee97792b9ba26719bcc16c7d7566ce2c
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-02 23:35:17 -0400

    new courses images

--- pages/courses.js
+++ pages/courses.js
@@ -207,6 +207,54 @@ export default function Courses() {
               <div className="p-4 sm:p-6 min-h-48">
                 <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
                   <span className="text-amber-500">
+                    Backend Development!
+                  </span>
+                </p>
+                <div className="flex flex-row">
+                  <p className="text-[#3C3C4399] text-2xl mr-2 line-through">
+                    $200
+                  </p>
+                  <p className="text-2xl font-bold text-[#0FB478]">FREE</p>
+                </div>
+                <p className="text-gray-700 text-xl mt-6">
+                  Develop Cloud Based Applications using Amazon AWS!
+                </p>
+
+                <a
+                  target=""
+                  referrerPolicy="origin"
+                  href="https://youtu.be/fhRHGAgvrh4?si=rcUi8V--c4QbD2pn"
+                  className="block mt-10 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform bg-slate-800 rounded-[14px] hover:bg-slate-500 focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
+                  aria-label="go to email sign up form"
+                >
+                  View On YouTube
+                </a>
+                {/* <a
+                target="_blank"
+                href="https://apps.apple.com/us/app/id1493631471"
+                className="block mt-1.5 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform rounded-[14px] hover:bg-[#F2ECE7] hover:text-[#000000dd] focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
+              >
+                Download app
+              </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        {/* JS and TS Course CARD  */}
+        <div className="font-bold text-xl mt-5 h-full">
+          <div className="w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden">
+            <div className="max-w-md mx-auto">
+              <Image
+                src={ComingSoon}
+                width={500}
+                height={500}
+                alt="Picture of the author"
+              />
+              <div className="p-4 sm:p-6 min-h-48">
+                <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
+                  <span className="text-amber-500">
                     Cloud Developer: React & Amazon AWS
                   </span>
                 </p>
