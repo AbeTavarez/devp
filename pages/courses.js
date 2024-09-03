1c80aeae87bff2253d372812fb98dbfa6da8c50d
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-02 23:18:30 -0400

    courses updates

--- pages/courses.js
+++ pages/courses.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { AppLayout } from "../components/AppLayout";
 import ComingSoon from "../public/assets/coming-soon-banner.png";
 import ReactShorts from "../public/React-Shorts-YouTube-Thumbnail.png";
+import GPT4 from '../public/assets/gpt-4.avif'
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import EmailSubscribeForm from "../components/EmailSubscribeForm";
 
@@ -14,7 +15,50 @@ export default function Courses() {
 
       <section className="flex justify-evenly flex-wrap">
         {/* CARD  */}
-        <div className="font-bold text-3xl mt-5 h-full">
+        <div className="font-bold text-xl mt-5 h-full">
+          <div className="w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden">
+            <div className="max-w-md mx-auto">
+              <Image
+                src={GPT4}
+                width={500}
+                height={500}
+                alt="Picture of the author"
+              />
+              <div className="p-4 sm:p-6 min-h-48">
+                <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
+                  Building Intelligent Bots 
+                </p>
+                <div className="flex flex-row">
+                  <p className="text-[#3C3C4399] text-2xl mr-2 line-through">
+                    $200
+                  </p>
+                  <p className="text-2xl font-bold text-[#0FB478]">FREE</p>
+                </div>
+                <p className="text-gray-700 text-xl mt-6">
+                  Develop Intelligent Bots using NextJS, and OpenAI GPT4 models. 
+                </p>
+
+                <a
+                  target="_blank"
+                  href="https://youtu.be/RnQvAZHArL0?si=EigoYUrdo1Lr5Jc8"
+                  className="block mt-10 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform bg-slate-800 rounded-[14px] hover:bg-slate-500 focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
+                  aria-label="open course on youtube website"
+                >
+                  View on YouTube
+                </a>
+                {/* <a
+                target="_blank"
+                href="https://apps.apple.com/us/app/id1493631471"
+                className="block mt-1.5 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform rounded-[14px] hover:bg-[#F2ECE7] hover:text-[#000000dd] focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
+              >
+                Download app
+              </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* CARD  */}
+        <div className="font-bold text-xl mt-5 h-full">
           <div className="w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden">
             <div className="max-w-md mx-auto">
               <Image
@@ -23,17 +67,61 @@ export default function Courses() {
                 height={500}
                 alt="Picture of the author"
               />
-              <div className="p-4 sm:p-6">
-                <p className="font-bold text-gray-700 text-[22px] leading-7 mb-1">
+              <div className="p-4 sm:p-6 min-h-48">
+                <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
+                  NextJS Crash Course
+                </p>
+                <div className="flex flex-row">
+                  <p className="text-[#3C3C4399] text-2xl mr-2 line-through">
+                    $200
+                  </p>
+                  <p className="text-2xl font-bold text-[#0FB478]">FREE</p>
+                </div>
+                <p className="text-gray-700 text-xl mt-6">
+                  Beginner friendly NextJS Course go From 0 to hero!
+                </p>
+
+                <a
+                  target="_blank"
+                  href="https://youtu.be/lfenDlOFycw?si=3CujfsC2OSSg3S0j"
+                  className="block mt-10 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform bg-slate-800 rounded-[14px] hover:bg-slate-500 focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
+                  aria-label="open course on youtube website"
+                >
+                  View on YouTube
+                </a>
+                {/* <a
+                target="_blank"
+                href="https://apps.apple.com/us/app/id1493631471"
+                className="block mt-1.5 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform rounded-[14px] hover:bg-[#F2ECE7] hover:text-[#000000dd] focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
+              >
+                Download app
+              </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* CARD  */}
+        <div className="font-bold text-xl mt-5 h-full">
+          <div className="w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden">
+            <div className="max-w-md mx-auto">
+              <Image
+                src={ReactShorts}
+                width={500}
+                height={500}
+                alt="Picture of the author"
+              />
+              <div className="p-4 sm:p-6 min-h-48">
+                <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
                   &quot;React Shorts&quot; Full React Course
                 </p>
                 <div className="flex flex-row">
-                  <p className="text-[#3C3C4399] text-[17px] mr-2 line-through">
+                  <p className="text-[#3C3C4399] text-2xl mr-2 line-through">
                     $200
                   </p>
-                  <p className="text-[17px] font-bold text-[#0FB478]">FREE</p>
+                  <p className="text-2xl font-bold text-[#0FB478]">FREE</p>
                 </div>
-                <p className="text-gray-700 font-[15px] mt-6">
+                <p className="text-gray-700 text-xl mt-6">
                   Beginner friendly React Course! Take a fresh looks at React.
                 </p>
 
@@ -58,7 +146,7 @@ export default function Courses() {
         </div>
 
         {/* JS and TS Course CARD  */}
-        <div className="font-bold text-3xl mt-5 h-full">
+        <div className="font-bold text-xl mt-5 h-full">
           <div className="w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden">
             <div className="max-w-md mx-auto">
               <Image
@@ -67,19 +155,19 @@ export default function Courses() {
                 height={500}
                 alt="Picture of the author"
               />
-              <div className="p-4 sm:p-6">
-                <p className="font-bold text-gray-700 text-[22px] leading-7 mb-1">
+              <div className="p-4 sm:p-6 min-h-48">
+                <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
                   <span className="text-amber-500">
                   Cloud Developer: React & Amazon AWS
                   </span>
                 </p>
                 <div className="flex flex-row">
-                  <p className="text-[#3C3C4399] text-[17px] mr-2 line-through">
+                  <p className="text-[#3C3C4399] text-2xl mr-2 line-through">
                     $200
                   </p>
-                  <p className="text-[17px] font-bold text-[#0FB478]">FREE</p>
+                  <p className="text-2xl font-bold text-[#0FB478]">FREE</p>
                 </div>
-                <p className="text-gray-700 font-[15px] mt-6">
+                <p className="text-gray-700 text-xl mt-6">
                   Develop Cloud Based Applications using Amazon AWS!
                 </p>
 
@@ -116,7 +204,7 @@ export default function Courses() {
         /> */}
 
         {/* JS and TS Course CARD  */}
-        <div className="font-bold text-3xl mt-5 h-full">
+        <div className="font-bold text-xl mt-5 h-full">
           <div className="w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden">
             <div className="max-w-md mx-auto">
               <Image
@@ -125,17 +213,17 @@ export default function Courses() {
                 height={500}
                 alt="Picture of the author"
               />
-              <div className="p-4 sm:p-6">
-                <p className="font-bold text-gray-700 text-[22px] leading-7 mb-1">
+              <div className="p-4 sm:p-6 min-h-48">
+                <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
                   <span className="text-blue-500">React Native: Mobile Development</span>
                 </p>
                 <div className="flex flex-row">
-                  <p className="text-[#3C3C4399] text-[17px] mr-2 line-through">
+                  <p className="text-[#3C3C4399] text-2xl mr-2 line-through">
                     $200
                   </p>
-                  <p className="text-[17px] font-bold text-[#0FB478]">FREE</p>
+                  <p className="text-2xl font-bold text-[#0FB478]">FREE</p>
                 </div>
-                <p className="text-gray-700 font-[15px] mt-6">
+                <p className="text-gray-700 text-xl mt-6">
                   Launch your own cross mobile apps for Android and iOS using React Native!
                 </p>
 
@@ -172,19 +260,19 @@ export default function Courses() {
                 height={500}
                 alt="Picture of the author"
               />
-              <div className="p-4 sm:p-6">
-                <p className="font-bold text-gray-700 text-[22px] leading-7 mb-1">
+              <div className="p-4 sm:p-6 min-h-48">
+                <p className="font-bold text-gray-700 text-xl leading-7 mb-1">
                   <span className="text-violet-400">
                   TypeScript for JavaScript Developers
                   </span>
                 </p>
                 <div className="flex flex-row">
-                  <p className="text-[#3C3C4399] text-[17px] mr-2 line-through">
+                  <p className="text-[#3C3C4399] text-2xl mr-2 line-through">
                     $200
                   </p>
-                  <p className="text-[17px] font-bold text-[#0FB478]">FREE</p>
+                  <p className="text-2xl font-bold text-[#0FB478]">FREE</p>
                 </div>
-                <p className="text-gray-700 font-[15px] mt-6">
+                <p className="text-gray-700 text-xl mt-6">
                   Learn fast by converting JavaScript apps to TypeScript apps!
                 </p>
 
